ae8b7c6e00a11cff50be03072a3d96f1ec297811
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-10-01 23:18:14 +0200

    adjustments

--- components.js
+++ components.js
@@ -24,9 +24,9 @@ const pages = [
 d3.select("#navigation")
 	.classed('row', true)
 	.append('div')
-	.classed('col-12', true)
+	.classed('col-12 d-flex flex-wrap', true)
 	.selectAll('a').data(pages).join('a')
-	.classed('navigation-link', true)
+	.classed('navigation-link my-3 my-md-0', true)
 	.attr('href',d=>d.url)
 	.text(d=>d.label)
 
@@ -34,23 +34,30 @@ d3.select("#footer")
 	.classed('row', true)
 	.classed('my-5', true)	
 	.html(`
-<div class="col-4">
+<div class="col-sm-6 col-md-6">
 	<h3 class="mb-3">Faculty</h3>
 	<p>Michele Mauri</p>
 	<p>Tommaso Elli</p>
 	<p>Andrea Benedetti</p>
 	<br/>
-	<p><a href="https://drawwithcode.github.io">Previous Editions</a></p>
+	
 </div>
-<div class="col-4">
+<div class="col-sm-6 col-md-6 text-md-right">
+	<h3 class="mb-3">&MediumSpace;</h3>
+	<img xstyle="margin:auto;" class="mt-1 mr-2 mr-sm-5" src="logo-density.svg" />
+	<img xstyle="margin:auto;" class="mt-1 " src="logo-poli.svg" />
+</div>
+
+	`);
+// <p><a href="https://drawwithcode.github.io">Previous Editions</a></p>
+{/* <div class="col-4 col-md-2 col-lg-1">
 	<h3 class="mb-3">&MediumSpace;</h3>
 	<img style="margin:auto;" src="logo-density.svg" />
 </div>
-<div class="col-4">
+<div class="col-8 col-md-4 col-lg-3">
 	<h3 class="mb-3">&MediumSpace;</h3>
 	<img style="margin:auto;" src="logo-poli.svg" />
-</div>
-	`);
+</div> */}
 
 	d3.select('#intro')
 		.classed('row my-5', true)
